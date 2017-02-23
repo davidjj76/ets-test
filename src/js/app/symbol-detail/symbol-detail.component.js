ae3485bacd2eb9d06d744817063bf0d2029e6354
David Jimenez <davidjj76@gmail.com>
2017-02-23 10:26:16 +0100

    Termino la redirección en caso de error, y la recarga de la lista de symbols

--- src/js/app/symbol-detail/symbol-detail.component.js
+++ src/js/app/symbol-detail/symbol-detail.component.js
@@ -6,9 +6,33 @@ angular.
   module('symbolDetail').
   component('symbolDetail', {
     templateUrl: './app/symbol-detail/symbol-detail.template.html',
-    controller: ['$routeParams', 'Symbol', 'appGlobals',
-      function SymbolDetailController($routeParams, Symbol, appGlobals) {
-        this.symbols = appGlobals.symbols;
-        this.symbol = Symbol.get({symbolId: $routeParams.symbolId});
+    controller: ['$routeParams', '$window', 'Symbol', 'appGlobals',
+      function SymbolDetailController($routeParams, $window, Symbol, appGlobals) {
+  		var self = this;
+	    Symbol.get({symbolId: $routeParams.symbolId}, 
+	    	function(symbol) {
+	    		self.symbol = symbol;
+		      	if(appGlobals.symbols.length) {
+	  		  		self.symbols = appGlobals.symbols;
+		      	} else {
+					Symbol.query(function(symbols) {
+			          appGlobals.symbols = symbols.map(function(symbol) {
+			            return { id: symbol.id, name: symbol.name}
+			          });
+			          self.symbols = appGlobals.symbols;
+					}, function() {
+			      		redirectTo('/#!/symbols');
+					})
+	    		}
+	    	},
+	    	function() {
+	      		redirectTo('/#!/symbols');
+	    	}
+	    );
+
+     	function redirectTo(route) {
+      		$window.location.href = route;	
+      	}
+
       }
   ]});
